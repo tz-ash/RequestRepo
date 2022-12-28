e585eefbdb857e92e32085b5ba18c410288ecccc
ashuroy345 <121546290+ashuroy345@users.noreply.github.com>
2022-12-28 21:40:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -459,7 +459,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/CT_Up_Datess'),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/MyBotzzupdate'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
