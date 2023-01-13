2a9e301f4dfb10f030888dfe123ba21a56800126
ashuroy345 <121546290+ashuroy345@users.noreply.github.com>
2023-01-13 08:23:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from pyrogram import types
 from aiohttp import web
 from plugins import web_server
 
-PORT = "8000"
+PORT = "8080"
 
 class Bot(Client):
 
@@ -44,10 +44,10 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
-        app = worker.AppRunner(await web_server())
+        app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        await worker.TCPSite(app, bind_address, PORT).start()
+        await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
 
